2aa055b73c4fcea82632923c0836ca5d9ca973de
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-08-17 16:15:25 +0545

    Image view and label added

--- Core-ML-Demo/ViewController.swift
+++ Core-ML-Demo/ViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let label: UILabel =  {
+        let label = UILabel()
+        label.textAlignment = .center
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
